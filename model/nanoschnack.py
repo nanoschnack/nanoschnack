6d5181cccd7035b026dd97f2dbf17e02373250f8
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2025-12-20 12:14:30 +0100

    model/nanoschnack: add jupytext percent pairing

---
+++ model/nanoschnack.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
+# ---
+
+# %% [markdown]
+# # NanoSchnack LLM
+#
+# ## Setup
+#
+# - Install dependencies.
+# - Verify that MPS is available (for Apple Silicon GPUs).
+
+# %%
+import torch
+torch.backends.mps.is_available()
+torch.backends.mps.is_built()
+
+# %%
+device = torch.device("mps")
+x = torch.randn(1024,1024, device=device)
+
+# %%
